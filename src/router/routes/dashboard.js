efcd2dc5815e78a6578d8555cab277de08f2eec5
zilviazz <zilvia524@gmail.com>
2023-06-14 23:00:18 +0700

    hapus beberapa file

--- src/router/routes/dashboard.js
+++ src/router/routes/dashboard.js
@@ -15,35 +15,11 @@ const dashboard = {
             meta: { auth: true },
         },
         {
-            path: "billing",
-            name: "Billing",
-            component: () => import('@/views/Dashboard/Billing.vue'),
-            meta: { auth: true },
-        },
-        {
             path: "todo",
             name: "ToDo",
             component: () => import('@/views/Dashboard/ToDo.vue'),
             meta: { auth: true },
         },
-        {
-            path: "profile",
-            name: "Profile",
-            component: () => import('@/views/Dashboard/Profile.vue'),
-            meta: { auth: true },
-        },
-        {
-            path: "form",
-            name: "Form",
-            component: () => import('@/views/Dashboard/Form.vue'),
-            meta: { auth: true },
-        },
-        {
-            path: "formadd",
-            name: "FormAdd",
-            component: () => import('@/views/Dashboard/FormAdd.vue'),
-            meta: { auth: true },
-        }
     ],
 };
 
